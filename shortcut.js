05abbf818504e078bbc02bbdf467d4f9650e1cff
Jon Kuperman <jon.kuperman@gmail.com>
2014-12-21 22:13:36 -0800

    stop using string concat in the event listener

--- shortcut.js
+++ shortcut.js
@@ -3,25 +3,23 @@
   document.addEventListener('keypress', function(e) {
 
     var shortcut = {
-      46:  'LoadItems',   // .
-      47:  'SearchItems', // /
-      106: 'NextItem',    // j
-      107: 'PrevItem',    // k
-      110: 'NewItem',     // n
-      111: 'OpenItem',    // o
+      46:  'shortcutLoadItems',   // .
+      47:  'shortcutSearchItems', // /
+      106: 'shortcutNextItem',    // j
+      107: 'shortcutPrevItem',    // k
+      110: 'shortcutNewItem',     // n
+      111: 'shortcutOpenItem',    // o
     }[e.keyCode];
 
     if ( typeof shortcut === undefined ) {
       return;
     }
 
-    var shortcutEvent = 'shortcut' + shortcut;
-
     if (window.CustomEvent) {
-      var event = new CustomEvent(shortcutEvent);
+      var event = new CustomEvent(shortcut);
     } else {
-      var event = document.createEvent(shortcutEvent);
-      event.initCustomEvent(shortcutEvent, true, true);
+      var event = document.createEvent(shortcut);
+      event.initCustomEvent(shortcut, true, true);
     }
 
     document.dispatchEvent(event);
