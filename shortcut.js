c7cf16373230a754f566b21cd92436a9e2e4f3ff
Jon Kuperman <jon.kuperman@gmail.com>
2015-01-20 23:33:41 -0800

    reverted back to youmightnotneedjquery approach

--- shortcut.js
+++ shortcut.js
@@ -10,15 +10,21 @@
   };
 
   document.addEventListener('keypress', function(e) {
-    var shortcut = keyCodeMap[e.keyCode];
     var tagName = document.activeElement.tagName;
+    var shortcut = keyCodeMap[e.keyCode];
+    var shortcutEvent;
 
     if ( typeof shortcut === undefined || (tagName && tagName !== 'BODY' )) {
         return;
     }
 
-    var shortcutEvent = document.createEvent('CustomEvent');
-    shortcutEvent.initCustomEvent(shortcut, false, false, null);
+    if (window.CustomEvent) {
+        shortcutEvent = new CustomEvent(shortcut);
+    } else {
+        shortcutEvent = document.createEvent('CustomEvent');
+        shortcutEvent.initCustomEvent(shortcut, false, false, null);
+    }
+
     document.dispatchEvent(shortcutEvent);
   });
 
